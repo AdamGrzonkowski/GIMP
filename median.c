5b276d50d670869cbc0e8ad24bbb28aaebc47e35
AdamGrzonkowski <245594@stud.umk.pl>
2016-01-15 01:53:53 +0100

    Update median.c

--- median.c
+++ median.c
@@ -353,49 +353,6 @@ compareNumbers (const void *a, const void *b)
 }
 
 
-// -------------------------- //
-//    Sorts the given array   //
-//  using Heapsort algorithm  //
-// in time O(n), memory O(1)  //
-// -------------------------- //
-static inline void 
-heapSort(gint array[], gint N) 
-{
-  gint n = N, i = n/2, parent, child;
-  gint t;
-  //
-  for (;;) { 		   // Loops until array is sorted 
-    if (i > 0) { 	   // First stage - Sorting the heap 
-      i--;                 // Save its index to i 
-      t = array[i];        // Save parent value to t 
-    } else {               // Second stage - Extracting elements in-place 
-      n--;                 // Make the new heap smaller 
-      if (n == 0) return;  // When the heap is empty, we are done 
-      t = array[n];        // Save last value (it will be overwritten) 
-      array[n] = array[0]; // Save largest value at the end of array
-    }
- 
-    parent = i;            // We will start pushing down t from parent
-    child = i*2 + 1;       // parent's left child
- 
-    // Sift operation - pushing the value of t down the heap
-    while (child < n) {
-      if (child + 1 < n  &&  array[child + 1] > array[child]) {
-	child++;                      // Choose the largest child
-      }
-      if (array[child] > t) {         // If any child is bigger than the parent
-	array[parent] = array[child]; // Move the largest child up
-	parent = child;               // Move parent pointer to this child
-	child = parent*2 + 1;         // Find the next child
-      } else {
-	break;                        // t's place is found
-      }
-    }
-    array[parent] = t;                // We save t in the heap
-  }
-}
-
-
 // ------------------------------ //
 //   Process each tile inputRow   //
 // ------------------------------ //
